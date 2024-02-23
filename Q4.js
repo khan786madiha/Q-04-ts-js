19a12983b5cbfc38ccabd404e797b8c455cca792
Madiha Khan <159880897+khan786madiha@users.noreply.github.com>
2024-02-23 03:33:01 -0800

    Q-04 Ts/Js & node.js of 45 assignments.
    Madiha Khan's NODEJS 45 Assignment Q-04.

---
+++ Q4.js
@@ -0,0 +1,7 @@
+"use strict";
+// 4.	Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log('Albert Einstein once said, “A person who never made a mistake never tried anything new.”');
+let famous_person = " Ali Ibn Abi Talib";
+console.log(`${famous_person} “Follow the guidance of Prophet Muhammad (Salallahu Alaihi Wasallam) for it is the best of guidance; Follow the Sunnah, for it is the best of ways.”`);
---
+++ Q4.ts
@@ -0,0 +1,9 @@
+// 4.	Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+console.log('Albert Einstein once said, “A person who never made a mistake never tried anything new.”');
+
+let famous_person: string = " Ali Ibn Abi Talib"
+
+console.log(`${famous_person} “Follow the guidance of Prophet Muhammad (Salallahu Alaihi Wasallam) for it is the best of guidance; Follow the Sunnah, for it is the best of ways.”`);
+
